8f436c432cb79a7db9586e3461068044d7def193
吴伋 <wuji@gs-robot.com>
2020-12-18 09:08:16 +0800

    完成 单次sql执行时间

--- execute_sql.py
+++ execute_sql.py
@@ -9,8 +9,8 @@ class QueryTime(object):
     def __init__(self, config_file='./config.yaml'):
         self.config_file = config_file
         self._db_config = self.get_yaml_data(self.config_file)
-        self.db = pymysql.connect(**self._db_config['config'])
-        self.cursor = self.db.cursor()
+        self._db = pymysql.connect(**self._db_config['config'])
+        self.cursor = self._db.cursor()
 
     @staticmethod
     def get_yaml_data(yaml_file):
@@ -27,7 +27,7 @@ class QueryTime(object):
         self.cursor.execute(self.sql_time)
         end_at = self.cursor.fetchone()[0]
         duration = end_at - begin_at
-        self.cursor.execute('SELECT sleep(1);')
+        self.cursor.execute('SELECT sleep(0.3);')
         return duration
 
 
@@ -37,3 +37,5 @@ if __name__ == '__main__':
 
     spend_time = query_time.get_query_time(sql)
     print(spend_time)
+
+# done 单次sql执行时间
